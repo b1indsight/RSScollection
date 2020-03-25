1a69c008949a14d0c83bf40065768a47cc1819ad
b1indsight <zerolingling0@gmail.com>
2020-03-25 20:59:37 +0800

    add Rssdemo

--- src/main/java/com/RSScollection/demo/RSScollection/mainController.java
+++ src/main/java/com/RSScollection/demo/RSScollection/mainController.java
@@ -2,6 +2,10 @@ package com.RSScollection.demo.RSScollection;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.RSScollection.demo.RSScollection.User;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 
 @Controller
 public class mainController {
@@ -33,7 +41,21 @@ public class mainController {
 
     @GetMapping(path="/")
     public String welcomePage(@RequestParam(name="name", required=false, defaultValue="World")
-        String namel){
-            return "index";
-    }
+        String namel,HttpServletRequest request){
+            String url = "https://pythonhunter.org/episodes/feed.xml";
+            try{
+                XmlReader reader = new XmlReader(new URL(url));
+                SyndFeed feed = new SyndFeedInput().build(reader);
+                System.out.println(feed.getTitle());
+                System.out.println("***********************************");
+                String title = feed.getTitle();
+                System.out.println(title);
+                request.setAttribute("title", title);
+                List<SyndEntry> entries =  feed.getEntries();
+                request.setAttribute("entries", entries);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+            return "index";}
 }
